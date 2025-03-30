7aad73b903ac02dcaf0e04ef1002fbace24b24e5
Daniel <johnmejillon96@gmail.com>
2025-03-30 21:26:59 +0200

    ejercicio 9

--- vue-exercises/src/components/ChildComponent.vue
+++ vue-exercises/src/components/ChildComponent.vue
@@ -1,10 +1,17 @@
 <template>
 <div>
     <button @click="greetParent">Saludar al padre</button>
+    <p v-if="parentMessage">Padre dice: {{ parentMessage }}</p>
 </div>
 </template>
-  
+
 <script setup>
-  const emit = defineEmits(['greet'])
-  const greetParent = () => emit('greet', '¡Hola papá!')
+import { inject } from 'vue'
+
+const parentMessage = inject('parentMessage')
+const receiveChildMessage = inject('receiveChildMessage')
+
+const greetParent = () => {
+receiveChildMessage('¡Hola papá!') 
+}
 </script>
--- vue-exercises/src/components/ParentComponent.vue
+++ vue-exercises/src/components/ParentComponent.vue
@@ -1,16 +1,24 @@
 <template>
 <div>
-    <ChildComponent 
-    @greet="(msg) => childMessage = msg" 
-    />
+    <button @click="sendToChild">Enviar mensaje al hijo</button>
+    <ChildComponent />
     <p v-if="childMessage">Hijo dice: {{ childMessage }}</p>
 </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
+<script setup>
+import { ref, provide } from 'vue'
 import ChildComponent from './ChildComponent.vue'
 
+const parentMessage = ref('')
 const childMessage = ref('')
 
+provide('parentMessage', parentMessage)
+provide('receiveChildMessage', (msg) => {
+childMessage.value = msg
+})
+
+const sendToChild = () => {
+parentMessage.value = 'Hola hijo'
+}
 </script>
